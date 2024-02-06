2423ea8d079033d2638d67dc806372b5eb8f923f
Nikita <110695423+fanat1kq@users.noreply.github.com>
2024-02-06 19:22:39 +0300

    Delete type-search-index.js

--- type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"AppTest"},{"p":"org.example","l":"Indications"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"Role"},{"p":"org.example","l":"User"}];updateSearchResults();
